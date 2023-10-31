67dab3bf670f57b84fa00bdb6bea3af847c859d9
YoruNoKen <yorunoken@skiff.com>
2023-10-31 06:00:47 +0300

    added discord card

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", app);
 
-function app() {
-  console.log("Hello, I love you :3");
+async function app() {
+  const data = await fetch("https://api.lanyard.rest/v1/users/372343076578131968").then((res) => res.json()).then(json => json.data)
+  console.log(data)
+  const user = data.discord_user
+
+  document.getElementById("name").innerText = `${user.display_name} (${user.username}#${user.discriminator})`
+  document.getElementById("miku").src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`
+  document.getElementById("status").innerText = data.discord_status
 }
