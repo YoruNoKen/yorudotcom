bef7c19cb06e2a94698da395676136527cb85476
Yoru <m.f.fetvaci@hotmail.com>
2023-10-31 22:35:38 +0300

    fixed discord

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", app);
 
 const discordStatuses = {
   online: "rgba(35, 165, 89, 1)",
-  idle: "rgba(255, 253, 161, 1)",
+  idle: "rgba(255, 177, 50, 1)",
   dnd: "rgba(240, 71, 71, 1)",
   offline: "rgba(128, 132, 142, 1)",
 };
@@ -14,7 +14,7 @@ async function app() {
   console.log(data);
   const user = data.discord_user;
 
-  document.getElementsByClassName("discord-card")[0].style.backgroundColor = discordStatuses[data.discord_status];
-  document.getElementById("name").innerText = `${user.username} - ${data.discord_status}`;
-  document.getElementById("miku").src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`;
+  console.log(discordStatuses[data.discord_status]);
+  document.querySelectorAll(".discord-card")[0].style.border = `4px solid ${discordStatuses[data.discord_status]}`;
+  document.getElementById("discordUser").innerText = `${user.username} - ${data.discord_status}`;
 }
